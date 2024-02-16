583f47a2224eafdfe8c08dd6413e4d8533f62649
AshtonStephens <ashton.cs.stephens@gmail.com>
2024-02-16 18:54:50 -0500

    fix stacks single node follower name.

--- lib/stacks/app.ts
+++ lib/stacks/app.ts
@@ -7,7 +7,7 @@ import * as config from "./lib/config/stacksConfig";
 
 import { StacksSingleNodeStack } from "./lib/single-node-stack";
 import { StacksCommonStack } from "./lib/common-stack";
-import { StacksHANodesStack } from "./lib/ha-nodes-stack";
+// import { StacksHANodesStack } from "./lib/ha-nodes-stack";
 
 const app = new cdk.App();
 cdk.Tags.of(app).add("Project", "AWSStacks");
@@ -17,19 +17,20 @@ new StacksCommonStack(app, "stacks-common", {
     env: { account: config.baseConfig.accountId, region: config.baseConfig.region },
 });
 
-new StacksSingleNodeStack(app, "stacks-single-node-2", {
-    stackName: `stacks-single-node-2-${config.baseNodeConfig.stacksNodeConfiguration}`,
+new StacksSingleNodeStack(app, "stacks-single-node", {
+    stackName: `stacks-single-node-${config.baseNodeConfig.stacksNodeConfiguration}`,
     env: { account: config.baseConfig.accountId, region: config.baseConfig.region },
     ...config.baseNodeConfig
 });
 
-new StacksHANodesStack(app, "stacks-ha-nodes", {
-    stackName: `stacks-ha-nodes-${config.baseNodeConfig.stacksNodeConfiguration}`,
-    env: { account: config.baseConfig.accountId, region: config.baseConfig.region },
-    ...config.baseNodeConfig,
-    ...config.haNodeConfig
-});
-
+// TODO: Add the high availability nodes.
+//
+// new StacksHANodesStack(app, "stacks-ha-nodes", {
+//     stackName: `stacks-ha-nodes-${config.baseNodeConfig.stacksNodeConfiguration}`,
+//     env: { account: config.baseConfig.accountId, region: config.baseConfig.region },
+//     ...config.baseNodeConfig,
+//     ...config.haNodeConfig
+// });
 
 // Security Check
 cdk.Aspects.of(app).add(
